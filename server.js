912a0c7d1a04a96bb0db4a84cb8772e847fd5324
Priyansh <pkhare264@gmail.com>
2026-03-14 17:59:52 +0530

    fix: add Vercel frontend URL to CORS allowed origins

--- server.js
+++ server.js
@@ -57,7 +57,8 @@ const allowedOrigins = [
   process.env.CLIENT_URL,
   'http://localhost:5173',
   'http://localhost:3000',
-  'http://localhost:8081'
+  'http://localhost:8081',
+  'https://hangoutz-frontend.vercel.app'
 ].filter(Boolean);
 
 app.use(cors({
